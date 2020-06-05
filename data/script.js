982518f3eccde23726ba702daf5f587abc9f58bc
José Roberto Azevedo <66346043+joseazevedo47@users.noreply.github.com>
2020-06-04 21:39:11 -0300

    Listagem múltipla
    Os arquivos conseguem ser listados de acordo com o gerador na sua respectiva coluna

--- data/script.js
+++ data/script.js
@@ -5,17 +5,23 @@ xml = new XMLHttpRequest();
 xml.open('GET', 'listFiles', true);
 xml.onreadystatechange = function () {
   if ((xml.readyState==4) && (xml.status==200)){
+    var alvoA = document.getElementsByClassName('cellA')
+    var alvoB = document.getElementsByClassName('cellB')
+    var alvoC = document.getElementsByClassName('cellC')
+    var alvoD = document.getElementsByClassName('cellD')
     var listString;
-    var dataList = [];
-    var alvo = document.getElementsByClassName("cell")
     listString = xml.responseText;
-    dataList = mkList(listString)
+    
+    var dataList = mkList(listString)
 
-    for (var i in dataList) {
-      alvo[i].innerHTML = dataList[i]
+    for (var i = 0; i < dataList.A.length; i++) {
+      alvoA[i].innerHTML = dataList.A[i]
+      alvoB[i].innerHTML = dataList.B[i]
+      alvoC[i].innerHTML = dataList.C[i]
+      alvoD[i].innerHTML = dataList.D[i]
     }
     
-
+    
   }
 }
 xml.send();
@@ -24,17 +30,34 @@ xml.send();
 function mkList (stringList) {
 
   var arrayList = stringList.split("|");
-  var dataList = [];
+  var dataListA = [];
+  var dataListB = [];
+  var dataListC = [];
+  var dataListD = [];
+  var gen;
 
   for (var i in arrayList){
-    
-
-    if (arrayList[i].startsWith("/dados/")) {
-      dataList.push(arrayList[i].slice(7))
+    gen = arrayList[i].slice(0, 8)
+    switch (gen) {
+      case "/dados/A":
+        dataListA.push(arrayList[i].slice(7));
+      break;
+      case "/dados/B":
+        dataListB.push(arrayList[i].slice(7));
+      break;
+      case "/dados/C":
+        dataListC.push(arrayList[i].slice(7));
+      break;
+      case "/dados/D":
+        dataListD.push(arrayList[i].slice(7));
+      break;
     }
-  
-  
   }
 
-  return dataList
+  return {
+    A: dataListA,
+    B: dataListB,
+    C: dataListC,
+    D: dataListD
+  }
 }
