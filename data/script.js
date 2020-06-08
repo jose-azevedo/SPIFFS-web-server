058022fec5ece92215baed9a356d6cc12c34764e
José Roberto Azevedo <66346043+joseazevedo47@users.noreply.github.com>
2020-06-08 20:25:26 -0300

    Comentários e Logo
    Adição de comentários e redimensionamento da logo, diminuindo o arquivo de 177 Kb para 26 Kb

--- data/script.js
+++ data/script.js
@@ -1,61 +1,69 @@
+var key = true;
 
 function listar() {
-var xml;
-xml = new XMLHttpRequest();
-xml.open('GET', 'listFiles', true);
-xml.onreadystatechange = function () {
-  if ((xml.readyState==4) && (xml.status==200)){
-    var tab = document.getElementById('dataCells')
-    var listString;
-    listString = xml.responseText;
-    
-    var dataList = mkList(listString)
+  if(key){  // Validação para que a lista seja carregada somente uma vez após carregar a página
+    var xml;
+    xml = new XMLHttpRequest();
+    xml.open('GET', 'listFiles', true); // Requisição HTTP do tipo GET na path "/listFiles" do servidor
+    xml.onreadystatechange = function () {
+      if ((xml.readyState==4) && (xml.status==200)) { // Quando receber um OK do servidor e a lista bruta, inicia o processamento da lista
+        key = false // Impedimento para que a lista seja carregada de novo
+        var tab = document.getElementById('dataCells')
+        var listString;
+        listString = xml.responseText; // Recebe a lista bruta de todos os arquivos em uma única string separados por um "|"
+        
+        var dataList = mkList(listString) // Função recebe a lista bruta e retorna um objeto com as propriedades relativas a lista de cada gerador 
 
-    for (var i = 0; i < dataList.A.length; i++) {
-      var newRow = document.createElement('tr')
-      tab.appendChild(newRow)
+        for (var i = 0; i < dataList.A.length; i++) { // Laço se repete enquanto houverem elementos nos vetores
+          var newRow = document.createElement('tr') // Criação de uma nova linha na tabela
+          tab.appendChild(newRow) // Anexação da nova linha ao corpo da tabela
 
-      var newCellA = document.createElement('td')
-      newCellA.setAttribute('class', 'cellA')
-      newRow.appendChild(newCellA)
-      document.getElementsByClassName('cellA')[i].innerHTML = dataList.A[i]
-      newCellA.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+          var newCellA = document.createElement('td') // Criação de uma nova célula
+          var newCellB = document.createElement('td')
+          var newCellC = document.createElement('td')
+          var newCellD = document.createElement('td')
 
-      var newCellB = document.createElement('td')
-      newCellB.setAttribute('class', 'cellB')
-      newRow.appendChild(newCellB)
-      document.getElementsByClassName('cellB')[i].innerHTML = dataList.B[i]
-      newCellB.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
-      
-      var newCellC = document.createElement('td')
-      newCellC.setAttribute('class', 'cellC')
-      newRow.appendChild(newCellC)
-      document.getElementsByClassName('cellC')[i].innerHTML = dataList.C[i]
-      newCellC.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
-      
-      var newCellD = document.createElement('td')
-      newCellD.setAttribute('class', 'cellD')
-      newRow.appendChild(newCellD)
-      document.getElementsByClassName('cellD')[i].innerHTML = dataList.D[i]
-      newCellD.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+          newCellA.setAttribute('class', 'cellA') // Definição do atributo classe do elemento célula
+          newCellB.setAttribute('class', 'cellB') // Passo importante para garantir que cada arquivo fique em seu devido lugar
+          newCellC.setAttribute('class', 'cellC')
+          newCellD.setAttribute('class', 'cellD')
+
+          newRow.appendChild(newCellA) // Anexação da célula
+          newRow.appendChild(newCellB)
+          newRow.appendChild(newCellC)
+          newRow.appendChild(newCellD)
+
+          // O texto da célula é definido como igual ao do elemento no vetor
+          document.getElementsByClassName('cellA')[i].innerHTML = dataList.A[i] 
+          document.getElementsByClassName('cellB')[i].innerHTML = dataList.B[i]
+          document.getElementsByClassName('cellC')[i].innerHTML = dataList.C[i]
+          document.getElementsByClassName('cellD')[i].innerHTML = dataList.D[i]
+
+          // Define-se que ao clicar na célula o seu texto, nome do arquivo, seja copiado para a caixa de texto do elemento Formulário. É preciso que isso seja feito para comunicar ao servidor qual o nome do arquivo se deseja baixar quando o formulário for enviado.
+          newCellA.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+          newCellB.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+          newCellC.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+          newCellD.setAttribute('onclick', 'document.getElementById("filename").setAttribute("value", this.innerText)')
+        }
+      }
     }
+    xml.send(); // Envio da requisição HTTP ao servidor
   }
-}
-xml.send();
 }
 
 function mkList (stringList) {
 
-  var arrayList = stringList.split("|");
-  var dataListA = [];
+  var arrayList = stringList.split("|"); // Divide a string em um vetor de strings separando os elementos pelo caracter "|". Agora cada elemento deste vetor é o nome de um arquivo
+  var dataListA = []; // Vetores que receberão os nomes dos arquivos relativos ao seu gerador
   var dataListB = [];
   var dataListC = [];
   var dataListD = [];
-  var gen;
+  var gen; // Variável que recebe o prefíxo do nome do arquivo que possibilita a distinção entre geradores
 
   for (var i in arrayList){
 
-    gen = arrayList[i].slice(0, 8)
+    gen = arrayList[i].slice(0, 8) // Variável recebe o prefixo contido nos 8 primeiros caracteres
+    // Condicional switch para atribuir à última posição de cada vetor o nome de arquivo correspondente ao seu gerador, sem o prefíxo de diretório
     switch (gen) {
       case "/dados/A":
         dataListA.push(arrayList[i].slice(7));
@@ -72,7 +80,7 @@ function mkList (stringList) {
     }
   }
 
-  return {
+  return { // retorna um objeto com as 4 propriedades sendo vetores relativos a lista de arquivos de cada gerador
     A: dataListA,
     B: dataListB,
     C: dataListC,
@@ -80,6 +88,7 @@ function mkList (stringList) {
   }
 }
 
+// O elemento de formulário automaticamente envia uma requisição HTTP ao servidor quando seu botão "submit" é clicado, enviando ao servidor o texto escrito em sua caixa de texto. Para reduzir o número de passos para baixar um arquivo, esta função é anexada ao corpo da tabela para que, assim que uma célula for clicada e seu texto copiado para a caixa de texto, o formulário seja enviado logo em seguida.
 function submit() {
-  document.getElementById('submit').click()
+  document.getElementById('submit').click() // O botão "submit" do formulário é clicado
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,13 @@ AsyncWebServer server(80);
 
 int i = 0;
 
+// Credenciais da rede WiFi, nome da rede e senha
 const char* ssid = "Azevedo";
 const char* password = "familiaea";
 
 void setup(){
 
+// Inicialização de recursos, WiFi, comunicação serial e sistema de armazenamento de memória flash interna
   WiFi.begin(ssid,password);
 
   Serial.begin(115200);
@@ -25,26 +27,31 @@ void setup(){
     return;
   }
 
+// Espera até que a rede esteja conectada
   while(WiFi.status()!=WL_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
   Serial.println("");
-  Serial.print("IP Adress: ");
+
+// Imprime na tela o IP atribuído dinamicamente
+  Serial.print("IP Adress: "); 
   Serial.println(WiFi.localIP());
 
+// Atribui uma URL fixa
   if(!MDNS.begin("monitoracaogedae")) {
     Serial.println("MDNS falhou");
     return;  
   }
-  
   MDNS.addService("http", "tcp", 80);
 
+// "Paths", ou rotas, a que o servidor responde ao receber uma requisição de um cliente. Na raiz "/" o servidor envia a página HTML. Como os arquivos de estilo, código JavaScript e logo estão referenciados na página HTML as requisições pedindo por esses arquivos são enviadas automaticamente e por isso é preciso haver uma resposta para cada uma delas.
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(SPIFFS, "/index.html", "text/html");});
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(SPIFFS, "/style.css", "text/css");});
   server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(SPIFFS, "/script.js", "text/javascript");});
-  server.on("/gedae.png", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(SPIFFS, "/gedae.png", "image/png");});
+  server.on("/logo.png", HTTP_GET, [](AsyncWebServerRequest *request) {request->send(SPIFFS, "/logo.png", "image/png");});
 
+// Descrito no código JavaScript, na página, ao se apertar o botão "Listar arquivos" o cliente envia uma requisição na rota "/listFiles", que executa a função a seguir
   server.on("/listFiles", HTTP_GET, [](AsyncWebServerRequest *request) {
     File dir = SPIFFS.open("/");
     File file = dir.openNextFile();
@@ -64,12 +71,12 @@ void setup(){
 
   server.on("/download", HTTP_POST, [](AsyncWebServerRequest *request) {
     String fname = "/dados/";
-    fname += request->arg("filename");
-    Serial.print("Download requisitado: ");
+    fname += request->arg("filename"); // No AsyncWebServer é preciso especificar o 'name' do argumento enviado, não o índice
+    Serial.print("Download requisitado: "); // O nome de um argumento ainda pode ser verificado com a sintaxe request->argName(índice)
     Serial.println(fname);
     request->send(SPIFFS, fname, "text/text", true);
     });  
-
+    
   server.begin();
 
 }
